7811771dfcbac9a0c6542580c1b09aae9b37cd52
Shivam Rungta <shivam08rungta@gmail.com>
2019-02-13 22:52:09 +0530

    short db url

--- utility/config.js
+++ utility/config.js
@@ -2,5 +2,5 @@
 //configurations
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://demo:demo@userms-shard-00-00-rqref.mongodb.net:27017,userms-shard-00-01-rqref.mongodb.net:27017,userms-shard-00-02-rqref.mongodb.net:27017/test?ssl=true&replicaSet=UserMS-shard-0&authSource=admin&retryWrites=true'
+    'mongoUrl': 'mongodb+srv://demo:demo@userms-rqref.mongodb.net/test?retryWrites=true'
 }
